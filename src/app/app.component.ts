d95bc733d11c18112f80250d08d4be31873a62c2
Camilo Restrepo <camilorg2017@gmail.com>
2023-03-20 17:52:06 -0500

    update apis and presentation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core';
-import { MatTableDataSource } from '@angular/material/table';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-interface CryptoPrice {
+interface Coin {
+  id: string;
   name: string;
-  price: number;
+  symbol: string;
+  image: string;
+  current_price: number;
+  market_cap: number;
+  market_cap_rank: number;
+  fully_diluted_valuation?: number;
+  total_volume: number;
+  high_24h: number;
+  low_24h: number;
+  price_change_24h: number;
+  price_change_percentage_24h: number;
+  market_cap_change_24h: number;
+  market_cap_change_percentage_24h: number;
+  circulating_supply: number;
+  total_supply?: number;
+  max_supply?: number;
+  ath: number;
+  ath_change_percentage: number;
+  ath_date: string;
+  atl: number;
+  atl_change_percentage: number;
+  atl_date: string;
+  roi?: {
+    times: number;
+    currency: string;
+    percentage: number;
+  };
+  last_updated: string;
 }
 
 @Component({
@@ -11,8 +39,17 @@ interface CryptoPrice {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  displayedColumns: string[] = ['name', 'price'];
-  dataSource: MatTableDataSource<CryptoPrice> =
-    new MatTableDataSource<CryptoPrice>();
+export class AppComponent implements OnInit {
+  data: Coin[] = [];
+
+  constructor(private http: HttpClient) {}
+  ngOnInit() {
+    this.http
+      .get<Coin[]>(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=5&page=1&sparkline=false'
+      )
+      .subscribe((data: Coin[]) => {
+        this.data = data;
+      });
+  }
 }
