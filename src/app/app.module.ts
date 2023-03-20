ce81b3e7957c89e789ed16f6f775dbd4a110b07a
Camilo Restrepo <camilorg2017@gmail.com>
2023-03-19 19:39:45 -0500

    update routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,10 @@ import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 
-import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CryptoPricesComponent } from './crypto-prices/crypto.prices.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-const routes: Routes = [
-  { path: 'crypto-prices', component: CryptoPricesComponent },
-];
 
 @NgModule({
   declarations: [AppComponent, CryptoPricesComponent],
@@ -21,7 +16,7 @@ const routes: Routes = [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    RouterModule.forRoot(routes),
+
     BrowserAnimationsModule,
     MatTableModule,
     MatPaginatorModule,
